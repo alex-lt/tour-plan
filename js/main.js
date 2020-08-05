56f4fc11a2bb551ff3ca0448a235e2610a09a9fd
alex-lt <alexandrchizhovl@gmail.com>
2020-08-06 02:11:53 +0500

    fix: bug with syncing to repository

--- js/main.js
+++ js/main.js
@@ -39,3 +39,85 @@ var menuButton = document.querySelector(".button-menu");
 menuButton.addEventListener("click", function(){
   document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
 });
+
+//send form
+
+//form - newsletter
+$(document).ready(function(){
+
+  $('#newsletter-form').on('submit',function(){
+
+     var that=$(this),
+         url=that.attr('action'),
+        type=that.attr('method'),
+       data={};
+
+   that.find('[name]').each(function(index,value){
+
+       var that=$(this),
+           name=that.attr('name'),
+           value=that.val();
+
+
+       data[name]=value;
+
+   });
+
+   $.ajax({
+
+       url:url,
+       type:type,
+       data:data,
+       success:function(response){
+
+           console.log(response);
+           alert(response);
+       }
+
+
+   });
+
+   return false;
+
+});
+});
+
+//form - footer
+$(document).ready(function(){
+
+  $('#footer-form').on('submit',function(){
+
+     var that=$(this),
+         url=that.attr('action'),
+        type=that.attr('method'),
+       data={};
+
+   that.find('[name]').each(function(index,value){
+
+       var that=$(this),
+           name=that.attr('name'),
+           value=that.val();
+
+
+       data[name]=value;
+
+   });
+
+   $.ajax({
+
+       url:url,
+       type:type,
+       data:data,
+       success:function(response){
+
+           console.log(response);
+           alert(response);
+       }
+
+
+   });
+
+   return false;
+
+});
+});
