fa2eb4c699ae47c5eb531884655e8443fd905593
alex-lt <alexandrchizhovl@gmail.com>
2020-08-09 00:29:29 +0500

    fix: bugs

--- js/main.js
+++ js/main.js
@@ -103,11 +103,15 @@ $(document).ready(function(){
 
   AOS.init();
 
-  //lazy load map
-  
-
-
   // lazy load img
  
+  function init() {
+    var imgDefer = document.getElementsByTagName('img');
+    for (var i=0; i<imgDefer.length; i++) {
+    if(imgDefer[i].getAttribute('data-src')) {
+    imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
+    } } }
+    window.onload = init;
+
 });
 
